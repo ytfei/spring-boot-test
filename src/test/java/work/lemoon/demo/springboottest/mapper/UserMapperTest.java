caacaca8349511b2b27897e1d9e1677330295f30
Evans <evans@lemoon.work>
2020-03-29 13:49:07 +0800

    add assertj-db deps

--- src/test/java/work/lemoon/demo/springboottest/mapper/UserMapperTest.java
+++ src/test/java/work/lemoon/demo/springboottest/mapper/UserMapperTest.java
@@ -1,5 +1,6 @@
 package work.lemoon.demo.springboottest.mapper;
 
+import org.assertj.db.type.Table;
 import org.junit.jupiter.api.Test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
@@ -9,6 +10,9 @@ import org.springframework.context.annotation.Import;
 import work.lemoon.demo.springboottest.dao.UserDao;
 import work.lemoon.demo.springboottest.service.dto.User;
 
+import javax.sql.DataSource;
+
+import static org.assertj.db.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -21,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  **/
 @MybatisTest /* 这个注解会替换配置文件中声明的数据源，使用一个内存数据库来做测试 */
 // 该注解表示不要使用 @MyBatisTest 声明的内存数据库，而是使用系统配置的数据库
-//@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Import(UserDao.class)
 // 如果 Mapper 类上面没有加 @Mapper 注解，则需要增加 @MapperScan
 //@MapperScan("work.lemoon.demo.springboottest.mapper")
@@ -32,6 +36,9 @@ class UserMapperTest {
 
     @Autowired
     private UserDao userDao;
+
+    @Autowired
+    private DataSource dataSource;
 
     @Test
     void countUser() {
@@ -46,9 +53,14 @@ class UserMapperTest {
 
         User u2 = user2Mapper.selectOne(1L);
         assertEquals(u.getName(), u2.getName());
+
+        // 因为事务不同，且事务在测试后回滚，所以这个使用场景略有不同，可以用于测试初始化的配置数据断言
+//        Table table = new Table(dataSource, "user");
+//        assertThat(table).hasNumberOfRows(1);
     }
 
     @Test
     void addUser() {
+
     }
 }
