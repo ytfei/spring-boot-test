ea6d66bffdea509ac88f1a5a983d46882a04866e
Evans <evans@lemoon.work>
2020-03-31 14:14:44 +0800

    usage of mybatisx plugin for IDEA

--- src/main/java/work/lemoon/demo/springboottest/mapper/UserMapper.java
+++ src/main/java/work/lemoon/demo/springboottest/mapper/UserMapper.java
@@ -1,9 +1,12 @@
 package work.lemoon.demo.springboottest.mapper;
 
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 import work.lemoon.demo.springboottest.service.dto.User;
 
+@Repository
 public interface UserMapper {
 
     @Select("select count(1) from user")
@@ -12,4 +15,5 @@ public interface UserMapper {
     @Insert("insert into user(name) values(#{name})")
     int addUser(User user);
 
+    void updateName(@Param("id") long id, @Param("name") String name);
 }
